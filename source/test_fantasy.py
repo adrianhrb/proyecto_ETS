700ba09a764db09023c16d0c27d356a56d17e0c9
adrianhrb <adrian.herrera.br@gmail.com>
2023-06-05 18:30:00 +0100

    Añadido test

---
+++ source/test_fantasy.py
@@ -0,0 +1,46 @@
+import pytest
+import fantasy
+
+
+fantasy.USERS = {}
+user1 = fantasy.User(1, "pepe", "1234")
+user2 = fantasy.User(1, "pepe", "1234")
+
+
+def test_user_building():
+    assert user1.username == "pepe"
+    assert user1.id == 1
+    assert user1.budget == 50_000_000
+    assert user1.log_status == False
+
+
+def test_corrected_id():
+    assert user2.id == 2
+
+
+def test_login():
+    user1.login("1234")
+    assert user1.log_status == True
+
+
+def test_logout():
+    user1.logout()
+    assert user1.log_status == False
+
+
+def test_login_wrong_answer():
+    user2.login("0")
+    assert user2.log_status == False
+
+
+def test_buying_driver():
+    user1.buy_driver("Fernando Alonso")
+    user1.buy_driver("Lance Stroll")
+    assert user1.team["drivers"] == ["Fernando Alonso", "Lance Stroll"]
+
+
+def test_excepcion_login():
+    user1.logout()
+    with pytest.raises(fantasy.LoginError) as err:
+        user1.buy_driver("Lance Stroll")
+    assert str(err.value) == "The user is not logged"
